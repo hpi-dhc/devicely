b95c70f3ff69b790166c3ccfaa4dfc1f091b44ad
jostmorgenstern <Jost.Morgenstern@student.hpi.uni-potsdam.de>
2020-09-27 23:21:29 +0200

    add write method for Empatica

--- devicely/empatica.py
+++ devicely/empatica.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 from .helpers import create_df
 
-class EmpaticaReader:
 
+class EmpaticaReader:
     signal_names = ['acc', 'bvp', 'eda', 'hr', 'temp']
     acc_names = ['acc_x', 'acc_y', 'acc_z']
 
@@ -43,7 +43,10 @@ class EmpaticaReader:
         signal_names.remove('acc')
         signal_names = signal_names + self.acc_names + ['acc_mag']
 
-        df = create_df(signal_names, signals, sample_freqs, start_times)
+        self.signal_names = signal_names
+        self.signals = signals
+        self.sample_freqs = sample_freqs
+        self.start_times = start_times
 
         # Read inter beat intervals
         if os.stat(self.filelist['ibi']).st_size != 0:
@@ -54,12 +57,45 @@ class EmpaticaReader:
                 split = re.split('[,\n]', f.read())
                 ibis = np.array(split[1:-1:2]).astype(float)
                 timedeltas = list(map(lambda x: pd.Timedelta(x, unit='s'), np.array(split[:-1:2]).astype(float)))
-                timestamps = list(map(lambda x: start_time + x, timedeltas))
-                df_ibi = pd.DataFrame({'ibi': ibis}, index=timestamps)
-            self.data = df.join(df_ibi, how='outer')
+                self.ibi_data = {
+                    "start_time": start_time,
+                    "ibis": ibis,
+                    "timedeltas": timedeltas
+                }
         else:
-            self.data = df
+            self.ibi_data = None
             print("IBI file is empty.")
+
+        self.data = self.get_dataframe(signal_names, signals, sample_freqs, start_times, ibi_data=self.ibi_data)
+
+    def get_dataframe(self, signal_names, signals, sample_freqs, start_times, ibi_data=None):
+        df = create_df(signal_names, signals, sample_freqs, start_times)
+        if ibi_data is not None:
+            ibi_timestamps = list(map(lambda x: ibi_data["start_time"] + x, ibi_data["timedeltas"]))
+            df_ibi = pd.DataFrame({'ibi': ibi_data["ibis"]}, index=ibi_timestamps)
+            df = df.join(df_ibi, how='outer')
+
+        return df
+
+    def write(self, path, as_dataframe=False):
+        if as_dataframe:
+            self.data.to_csv(path_or_buf=path)
+        else:
+            empatica_path = os.path.join(path, "Empatica")
+            os.mkdir(empatica_path)
+            for signal_name in ['acc', 'bvp', 'eda', 'hr', 'temp']:
+                file_path = os.path.join(empatica_path, f"{signal_name.upper()}.csv")
+                with open(file_path, 'w') as f:
+                    if signal_name == "acc":
+                        starting_times = [self.start_times[axis] for axis in self.acc_names]
+                        sample_freqs = [self.sample_freqs[axis] for axis in self.acc_names]
+                        signals = np.array([self.signals[axis] for axis in self.acc_names]).T
+                        acc_data = np.row_stack([starting_times, sample_freqs, signals])
+                        pd.DataFrame(acc_data).to_csv(file_path, header=None, index=None)
+                    else:
+                        f.write(str(self.start_times[signal_name]) + '\n')
+                        f.write(str(self.sample_freqs[signal_name]) + '\n')
+                        f.write('\n'.join([str(x) for x in self.signals[signal_name]]))
 
     def init_filelist(self, path):
         self.filelist = {
