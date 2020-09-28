e7ceb66fad64bc72cc78eed37b0578a3f86c7341
jostmorgenstern <Jost.Morgenstern@student.hpi.uni-potsdam.de>
2020-09-28 12:47:44 +0200

    store signals as timestamps and timedeltas internally

--- devicely/empatica.py
+++ devicely/empatica.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import re
 import numpy as np
@@ -12,61 +13,53 @@ class EmpaticaReader:
 
     def __init__(self, path):
         self.init_filelist(path)
-        signals = {}
-        start_times = {}
-        sample_freqs = {}
+        self.signals = {}
+        self.start_times = {}
+        self.sample_freqs = {}
 
         for signal in self.signal_names:
             filename = self.filelist[signal]
-            with open(filename, 'r') as f:
-                ts_line = f.readline()
-                start_times[signal] = int(ts_line[:ts_line.find('.')])
-                sf_line = f.readline()
-                sample_freqs[signal] = int(sf_line[:sf_line.find('.')])
-                split = re.split('[,\n]', f.read())
-                split = np.array(split[:-1]).astype(float)
-                if signal == 'acc':
-                    for index, axis in enumerate(self.acc_names):
-                        signals[axis] = split[index::3]
-                        sample_freqs[axis] = sample_freqs[signal]
-                        start_times[axis] = start_times[signal]
-                    del start_times[signal]
-                    del sample_freqs[signal]
-                    continue
-                signals[signal] = split
+            split = pd.read_csv(filename, header=None).to_numpy(dtype="float")
+            if signal == 'acc':
+                for index, axis in enumerate(self.acc_names):
+                    self.start_times[axis] = pd.Timestamp(split[0,index], unit='s')
+                    self.sample_freqs[axis] = split[1,index]
+                    self.signals[axis] = split[2:,index]
+            else:
+                self.start_times[signal] = pd.Timestamp(split[0,0], unit='s')
+                self.sample_freqs[signal] = split[1,0]
+                self.signals[signal] = split[2:,0]
 
-        signals['acc_mag'] = np.linalg.norm([signals['acc_x'], signals['acc_y'], signals['acc_z']], axis=0)
-        sample_freqs['acc_mag'] = sample_freqs['acc_x']
-        start_times['acc_mag'] = start_times['acc_x']
+            #signals[signal] = split
 
-        signal_names = self.signal_names.copy()
-        signal_names.remove('acc')
-        signal_names = signal_names + self.acc_names + ['acc_mag']
-
-        self.signal_names = signal_names
-        self.signals = signals
-        self.sample_freqs = sample_freqs
-        self.start_times = start_times
+        self.signals['acc_mag'] = np.linalg.norm([self.signals['acc_x'], self.signals['acc_y'], self.signals['acc_z']], axis=0)
+        self.sample_freqs['acc_mag'] = self.sample_freqs['acc_x']
+        self.start_times['acc_mag'] = self.start_times['acc_x']
 
         # Read inter beat intervals
         if os.stat(self.filelist['ibi']).st_size != 0:
-            ibi_filename = self.filelist['ibi']
-            with open(ibi_filename, 'r') as f:
-                ts_line = f.readline()
-                start_time = pd.to_datetime(int(ts_line[:ts_line.find('.')]), unit='s')
-                split = re.split('[,\n]', f.read())
-                ibis = np.array(split[1:-1:2]).astype(float)
-                timedeltas = list(map(lambda x: pd.Timedelta(x, unit='s'), np.array(split[:-1:2]).astype(float)))
-                self.ibi_data = {
-                    "start_time": start_time,
-                    "ibis": ibis,
-                    "timedeltas": timedeltas
-                }
+            ibi_df = pd.read_csv(self.filelist['ibi'], header=None)
+            start_time = pd.Timestamp(ibi_df.iloc[0,0], unit='s')
+            timedeltas = pd.to_timedelta(ibi_df.iloc[1:,0], unit='s')
+            ibis = pd.to_timedelta(ibi_df.iloc[1:,1].astype('float'), unit='s')
+
+            self.ibi_data = {
+                "start_time": start_time,
+                "ibis": ibis,
+                "timedeltas": timedeltas
+            }
+
         else:
             self.ibi_data = None
             print("IBI file is empty.")
 
-        self.data = self.get_dataframe(signal_names, signals, sample_freqs, start_times, ibi_data=self.ibi_data)
+        a = 0
+
+        #self.data = self.get_dataframe(signal_names, signals, sample_freqs, start_times, ibi_data=self.ibi_data)
+
+    def timeshift(self, timeshift=0):
+        pass
+
 
     def get_dataframe(self, signal_names, signals, sample_freqs, start_times, ibi_data=None):
         df = create_df(signal_names, signals, sample_freqs, start_times)
@@ -83,7 +76,7 @@ class EmpaticaReader:
         else:
             empatica_path = os.path.join(path, "Empatica")
             os.mkdir(empatica_path)
-            for signal_name in ['acc', 'bvp', 'eda', 'hr', 'temp']:
+            for signal_name in self.signal_names:
                 file_path = os.path.join(empatica_path, f"{signal_name.upper()}.csv")
                 with open(file_path, 'w') as f:
                     if signal_name == "acc":
