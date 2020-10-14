1c3f4e3077576de444bef832fa92d89dd8c4ff81
Ariane Sasso <ariane.sasso@gmail.com>
2020-10-14 10:44:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'Devicely'
+NAME = 'devicely'
 DESCRIPTION = 'Read multiple signals from different sources.'
 URL = None
 EMAIL = 'felix.musmann@student.hpi.de, ariane.morassi-sasso@hpi.de'
